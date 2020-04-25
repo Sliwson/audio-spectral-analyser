466833a7f96a1c0deb864c2555a8bcf619362f9b
Mateusz Śliwakowski <mateusz_sliw@interia.pl>
2020-04-25 16:00:09 +0200

    Init empty winforms project

---
+++ audio-spectral-analyser/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace audio_spectral_analyser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
